e5af00a9426cf117f42676db2e8ff80f49175caa
salwaparacha <salwaparacha1999@gmail.com>
2026-09-14 18:15:16 +0400

    deploying initial nodejs app

--- test/app.test.js
+++ test/app.test.js
@@ -1,3 +1,4 @@
+
 const test = require('node:test');
 const assert = require('node:assert');
 const { add } = require('../app');
@@ -5,3 +6,4 @@ const { add } = require('../app');
 test('addition works correctly', () => {
     assert.strictEqual(add(2, 3), 5);
 });
+
