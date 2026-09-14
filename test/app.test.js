7a56aa22ec01adc983992c321160ce22624a1ce0
salwaparacha <salwaparacha1999@gmail.com>
2026-09-14 18:01:19 +0400

    Initial Node.js application

---
+++ test/app.test.js
@@ -0,0 +1,7 @@
+const test = require('node:test');
+const assert = require('node:assert');
+const { add } = require('../app');
+
+test('addition works correctly', () => {
+    assert.strictEqual(add(2, 3), 5);
+});
