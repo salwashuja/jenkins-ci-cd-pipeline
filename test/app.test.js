fc47f3c8f916d5cc53fb1eb3f42460f7e64bde10
salwaparacha <salwaparacha1999@gmail.com>
2026-09-15 22:13:26 +0400

    Fixed the error

--- test/app.test.js
+++ test/app.test.js
@@ -4,6 +4,6 @@ const assert = require('node:assert');
 const { add } = require('../app');
 
 test('addition works correctly', () => {
-    assert.strictEqual(add(2, 3), 5;
+    assert.strictEqual(add(2, 3), 5);
 });
 
